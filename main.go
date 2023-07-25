ca5ffe1d75a3606d455473cd80177a6fc85c379c
SergeyTyurin <bwert@yandex.ru>
2023-07-25 10:38:57 +0000

    fix golangci.yml

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/SergeyTyurin/banner_rotation/configs"
 	"github.com/SergeyTyurin/banner_rotation/database"
@@ -30,30 +31,35 @@ func main() {
 
 	msgConfig, err := configs.GetMessageBrokerConfig("config/connection_config.yaml")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 
 	broker := message_broker.NewBroker()
 	closeBroker, err := broker.Connect(msgConfig)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	defer closeBroker()
 
 	appConfig, err := configs.GetAppSettings("config/connection_config.yaml")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 	// Создание сервера с мультиплексором запросов
 	muxRouter := router.NewRouter(db, broker)
 	server := http.Server{
-		Addr:    fmt.Sprintf("%s:%d", appConfig.Host(), appConfig.Port()),
-		Handler: muxRouter.Mux(),
+		Addr:              fmt.Sprintf("%s:%d", appConfig.Host(), appConfig.Port()),
+		Handler:           muxRouter.Mux(),
+		ReadHeaderTimeout: 30 * time.Second,
 	}
 	defer server.Close()
 	log.Println("listening...")
 	// Прослушивание сервера
 	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
 }
